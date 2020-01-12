ec38c5daebfcee991fe6d11bb77eebfefa419362
Mustafa Dalğa <mustafadalgaa@gmail.com>
2020-01-13 02:33:21 +0300

    Code optimization

--- assets/js/tactix.js
+++ assets/js/tactix.js
@@ -444,6 +444,6 @@ function modalEffect() {
 
 function checkInput(value) {
   var lt = /</g, gt = />/g, ap = /'/g, ic = /"/g;
-  return value.toString().replace(lt, "&lt;").replace(gt, "&gt;").replace(ap, "&#39;").replace(ic, "&#34;");
+  return value.toString().replace(lt, "").replace(gt, "").replace(ap, "").replace(ic, "");
 }
 
